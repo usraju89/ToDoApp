711cd1b0381f4ed186c0dfc190d7039dd172b60e
SATHI RAJU UPPALAPATI <SATHI RAJU UPPALAPATI>
2018-12-23 12:50:57 -0600

    Beautyfy

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import TodoItem from './TodoItem'
 
 class Todo extends Component {
@@ -12,37 +12,37 @@ class Todo extends Component {
             }
         ]
     }
-    constructor(props){
-     super(props);
-     this.addTodo = this.addTodo.bind(this);
-     this.newTodoChange = this.newTodoChange.bind(this);
-     this.changeComplete = this.changeComplete.bind(this);
+    constructor(props) {
+        super(props);
+        this.addTodo = this.addTodo.bind(this);
+        this.newTodoChange = this.newTodoChange.bind(this);
+        this.changeComplete = this.changeComplete.bind(this);
     }
-    addTodo(){
-       this.setState({ 
-           todos: [ ...this.state.todos, { id: new Date().getTime(),todo: this.state.newTodo } ],
-           newTodo : ""
-     })
-    }
-    newTodoChange(event){
+    addTodo() {
         this.setState({
-            newTodo : event.target.value
+            todos: [...this.state.todos, { id: new Date().getTime(), todo: this.state.newTodo }],
+            newTodo: ""
+        })
+    }
+    newTodoChange(event) {
+        this.setState({
+            newTodo: event.target.value
         });
     }
-    changeComplete(){
-      alert("changeComplete");
+    changeComplete() {
+        alert("changeComplete");
     }
- render(){
-     return(
-        <div>
-            <input type="text" value={this.state.newTodo} onChange={this.newTodoChange}/>
-            <button onClick={this.addTodo}>Add ToDo</button>
-            {this.state.todos.map((todo)=>{
-                return <TodoItem todo = {todo} key={todo.id} changeComplete={this.changeComplete}/>
-            })}
-        </div>
-     );
- }
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.newTodo} onChange={this.newTodoChange} />
+                <button onClick={this.addTodo}>Add ToDo</button>
+                {this.state.todos.map((todo) => {
+                    return <TodoItem todo={todo} key={todo.id} changeComplete={this.changeComplete} />
+                })}
+            </div>
+        );
+    }
 
 }
 
